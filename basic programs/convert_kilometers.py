fc2858d82715704d877b7295d0ec2b08cc258803
devika49 <devikajo99@gmail.com>
2025-08-29 15:15:36 +0530

    Converting kilometers to Miles code using python

---
+++ basic programs/convert_kilometers.py
@@ -0,0 +1,6 @@
+kilometers = float(input("Enter the value of Kilometers: "))
+conversion_factor = 0.621371
+miles = kilometers * conversion_factor
+miles_round = round(miles,2)
+print(f"{kilometers} kilometers is equal to {miles_round} miles")
+
